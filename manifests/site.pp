bd624baa5787d658400027d6d0b7d5f0ee3f346d
Tommy Speigner <tspeigner@gmail.com>
2016-10-25 12:11:41 -0700

    Managing motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   notify { 'This is my new code': }
+  
+  class motd {
+    content => 'This is my new content for MOTD, have a great day'
+    owner   => 'root'
+  }
 }
