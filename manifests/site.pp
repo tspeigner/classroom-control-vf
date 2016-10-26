4987f4a453bf746bf94fb75cb79643001d1685de
Tommy Speigner <tspeigner@gmail.com>
2016-10-26 11:58:28 -0700

    memcached inlcude

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,9 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node tspeigner.puppetlabs.vm {
+  inlcude memcached
+  }
 
 node default {
   # This is where you can declare classes for all nodes.
