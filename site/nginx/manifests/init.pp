0c1212dfb971ab0106bbff1891a23d92d84a320c
Tommy Speigner <tspeigner@gmail.com>
2016-10-26 14:06:23 -0700

    added commas

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -12,41 +12,40 @@ class nginx {
     owner   => 'root',
     group   => 'root',
     source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx']
-    notify  => Service['nginx']
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
-  
+
     file { '/etc/nginx/nginx.conf':
       ensure  => file,
       mode    => '0644',
       owner   => 'root',
       group   => 'root',
       source  => 'puppet:///modules/nginx/nginx.conf',
-      require => Package['nginx']
-      notify  => Service['nginx']
-    }
-  
-    file { '/var/www':
-      ensure  => directory,
-      require => Package['nginx']
-      notify  => Service['nginx']
+      require => Package['nginx'],
+      notify  => Service['nginx'],
     }
 
-    
+    file { '/var/www':
+      ensure  => directory,
+      require => Package['nginx'],
+      notify  => Service['nginx'],
+    }
+
+
     file { '/var/www/index.html':
       ensure  => file,
       mode    => '0644',
       owner   => 'root',
       group   => 'root',
       source  => 'puppet:///modules/nginx/index.html',
-      require => Package['nginx']
-      notify  => Service['nginx']
+      require => Package['nginx'],
+      notify  => Service['nginx'],
       }
-    
+
   service { 'nginx':
-      ensure    => running,
-      enable    => true,
-      subscribe => 
+      ensure => running,
+      enable => true,
 
   }
 
