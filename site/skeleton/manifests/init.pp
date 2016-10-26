ea7b36e261909fe6cbd80a2588ab0954b40171c9
Tommy Speigner <tspeigner@gmail.com>
2016-10-26 08:59:17 -0700

    changed layout

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,17 @@
 #new skeleton class
 class skeleton {
+  file { '/etc/skel/':
+  ensure => 'directory',
+  group  => 'root',
+  mode   => '0755',
+  owner  => 'root',
+  }
+
   file { '/etc/skel/.bashrc':
-  ensure  => file,
-  source  => "puppet:///modules/skeleton/bashrc",
- }
+  ensure  => 'file',
+  group   => 'root',
+  mode    => '0644',
+  owner   => 'root',
+  }
+
 }
